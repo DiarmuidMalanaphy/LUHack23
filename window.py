2ed7382cf2973d8392c35bf909573fadbf39ec32
Gillian Zech <ginger01@email.unc.edu>
2023-11-25 15:23:43 +0000

    window

--- window.py
+++ window.py
@@ -1,10 +1,7 @@
-import cv2
-
-
 class window:
     #def __init__()
     def __init__(self):
         pass
 
     def display(self, screen):#override this and implement it yourself
-        raise "Implement yourself"
+        raise Exception("Implement yourself")
