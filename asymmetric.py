c2f0261c23aa0acbbde315ad56bf2f876e650106
Gillian Zech <ginger01@email.unc.edu>
2023-11-25 15:03:41 +0000

    more windows

--- asymmetric.py
+++ asymmetric.py
@@ -1,9 +1,11 @@
 import pygame
 import window
-class asymmetric(window):
+class asymmetric(window.window):
     def __init__(self, screen):
         self.backgroundImg = screen.fill("gray")
+        self.font = pygame.font.Font("Pixtura12.ttf", 36)
+        self.text = "Asymmetric puzzle text here"
 
     def display(self,screen):
-        self.screen.blit(self.image, (screen.height/4, 0))
-        self.screen.blit(text, (SCREEN_WIDTH/4+30,30))
+        screen.blit(self.backgroundImg, (screen.get_height()/4, 0))
+        screen.blit(self.text, (screen.get_width()/4+30,30))
